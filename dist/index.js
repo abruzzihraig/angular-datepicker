25409bf6c1787e1b43d30261690aac2618f90e42
何洋 <yang.he@jimubox.com>
2015-06-15 18:33:24 +0800

    Revert "Revert "update: compiled the dropdown directive""
    This reverts commit 8f1914fc782713b5bc0298e0d5eb885afc00bb0f.

--- dist/index.js
+++ dist/index.js
@@ -467,6 +467,29 @@ Module.directive('dateRange', function () {
 
 'use strict';
 
+var Module = angular.module('datePicker');
+
+Module.directive('dropdownDatepicker', ['$document', function($document) {
+  return {
+    restrict: 'ACE',
+    link: function(scope, element) {
+      element.bind('click', function(e) {
+        e.preventDefault();
+        e.stopPropagation();
+        element.addClass('open');
+      });
+      scope.$on('hideDropdownPicker', function() {
+        element.removeClass('open');
+      });
+      $document.bind('click', function() {
+        element.removeClass('open');
+      });
+    }
+  };
+}]);
+
+'use strict';
+
 var PRISTINE_CLASS = 'ng-pristine',
     DIRTY_CLASS = 'ng-dirty';
 
